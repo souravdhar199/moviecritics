f97384c467c376fd148e49843757b90205547a55
sourav199 <souravdhar1999@gmail.com>
2023-09-08 23:30:56 -0400

    added movie  posted and name

--- lib/pages/secondpage.dart
+++ lib/pages/secondpage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SecondPage extends StatefulWidget {
   const SecondPage({super.key});
@@ -8,25 +11,67 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  List data = [];
+  Future<String> getreq() async {
+    var api = Uri.https("api.themoviedb.org", "/3/discover/movie",
+        {"api_key": "1e058d9d1bbb0bf7e45949d02e6d8f7d"});
+    var response = await http.get(api);
+
+    Map<String, dynamic> map = jsonDecode(response.body);
+
+    data = map["results"];
+
+    print(data);
+
+    return response.body.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("This is second page"),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
-                child: const Text("Home"))
-          ],
-        ),
-      ),
-    );
+        child: FutureBuilder(
+      future: getreq(),
+      builder: (context, snapshot) {
+        //if we are waiting for the response
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+              child: Text(
+            "Waiting...",
+            style: TextStyle(fontSize: 43, fontWeight: FontWeight.w300),
+          ));
+        } else if (snapshot.hasError) {
+          return const Center(
+              child: Text(
+            "Hold on we fixing it 🔧",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+          ));
+        } else {
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(1),
+                child: Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                          "https://image.tmdb.org/t/p/w300${data[index]["backdrop_path"]}"),
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text(data[index]["title"]),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+            itemCount: data.length,
+          );
+        }
+      },
+    ));
   }
 }
